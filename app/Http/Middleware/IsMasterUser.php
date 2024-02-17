4603f4defbfbcaaec666c6d432becd12cd1b1b37
LeiteEduardo <duleiteee@gmail.com>
2024-02-17 11:26:29 -0300

    feat - role middleware

---
+++ app/Http/Middleware/IsMasterUser.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+use Symfony\Component\HttpFoundation\Response;
+
+class IsMasterUser
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check() && Auth::user()->role == 'master') {
+            return $next($request);
+        }
+        Session::flash('warning', 'Your profile does not have permissions to access this page.');
+        return redirect('home');
+    }
+}
